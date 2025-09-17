ff0f58ae463cdfcd42ed66d5beec741f2d47f599
Dmitry Krivoruchko <144147209+UmbrellaLeaf5@users.noreply.github.com>
2025-09-17 22:51:02 +0300

    init Kotlin Spring Boot Project

---
+++ src/main/kotlin/io/github/umbrellaleaf5/tasks_organizer/TasksOrganizerApplication.kt
@@ -0,0 +1,12 @@
+package io.github.umbrellaleaf5.tasks_organizer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+
+@SpringBootApplication
+class TasksOrganizerApplication
+
+fun main(args: Array<String>) {
+  runApplication<TasksOrganizerApplication>(*args)
+}
